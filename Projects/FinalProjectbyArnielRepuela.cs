8a1fd4d44362a93861e6877c6462cd4dd4c8f853
Arniel Repuela <153043051+repuela123@users.noreply.github.com>
2023-12-18 13:07:28 +0800

    ConstructionSalaryFinalProjectbyArnielRepuela.cs

--- Projects/FinalProjectbyArnielRepuela.cs
+++ Projects/FinalProjectbyArnielRepuela.cs
@@ -1,136 +1,53 @@
-﻿using System;
-namespace FinalRepuela
+using System;
+
+class ConstructionSalaryCalculator
 {
-    class Program
+    const decimal DefaultHourlyRate = 20;
+
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Welcome to the Construction Salary Calculator!");
+        Console.WriteLine("--------------------------------------------");
+
+        Console.Write("Enter the number of workers: ");
+        if (int.TryParse(Console.ReadLine(), out int numberOfWorkers) && numberOfWorkers > 0)
         {
-            //Module 1.
-            Console.WriteLine("Module 1.\nWelcome to my Final Project in OOP");
-            int x = 15, y = 12;
-            double z = 1.1;
-            char d = 'd';
-            bool isFalse = false;
-            Console.WriteLine(x + y);
-            Console.WriteLine(d + "\n" + z + "\n" + isFalse + "\n" + x + "\n" + y + "\n");
-            //Module 2
-            Console.WriteLine("Module 2.");
-            //Arithmetic Operators
-            int a = 10;
-            int b = 3;
-            int sum = a + b;
-            int difference = a - b;
-            int product = a * b;
-            int quotient = a / b;
-            int remainder = a % b;
-            int num = 5;
-            num++;
-            num--;
-            Console.WriteLine(x);
-            //Assignment Operator
-            int numm = 5;
-            numm += 3;
-            numm -= 3;
-            numm /= 3;
-            numm *= 3;
-            numm %= 3;
-            Console.WriteLine(num + "\nEnter your name: ");
-            string message = @",Welcome to Florida city!
-            Hey There";
-            string user = Console.ReadLine();
-            Console.WriteLine($"Hi {user} " + message + "\t :0");
+            for (int i = 1; i <= numberOfWorkers; i++)
+            {
+                Console.WriteLine($"\nWorker {i}:");
 
-            //Module 3
-            Console.WriteLine($"Module 3.\nEnter your age {user}");
-            int age = Convert.ToInt32(Console.ReadLine());
-            if (age == 0)
-            {
-                Console.WriteLine("Baby");
-            }
-            else if (age > 1 && age <= 17)
-            {
-                Console.WriteLine("Child");
-            }
-            else if (age >= 18 && age <= 40)
-            {
-                Console.WriteLine("Adult");
-            }
-            else
-            {
-                Console.WriteLine("Old");
-            }
-            Console.WriteLine("Choose your Destination: \n 1.Florida \n 2.Canada \n 3. New york \n 4. None");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
-            {
-                case 1:
-                    Console.WriteLine("Florida");
-                    break;
-                case 2:
-                    Console.WriteLine("Canada");
-                    break;
-                case 3:
-                    Console.WriteLine("New york");
-                    break;
-                default:
-                    Console.WriteLine("None");
-                    break;
+                Console.Write("Enter the name of the worker: ");
+                string workerName = Console.ReadLine();
 
-            }
-            int ex = 100;
-            int ey;
-            ey = (ex > 10) ? 20 : 30;
-            Console.WriteLine(ey);
-            //Module 4.
-            //For Loop
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(i);
-            }
-            int r = 0;
-            while (r < 5)
-            {
-                Console.WriteLine(r);
-                r++;
-            }
-            //Break and Continue
-            for (int i = 0; i <= 10; i++)
-            {
-                if (i == 5)
+                Console.Write("Enter the number of hours worked this week: ");
+                if (int.TryParse(Console.ReadLine(), out int hoursWorked) && hoursWorked >= 0)
                 {
-                    break;
+                    Console.Write("Enter the hourly rate (press Enter to use the default rate): ");
+                    string rateInput = Console.ReadLine();
+                    decimal hourlyRate = string.IsNullOrWhiteSpace(rateInput) ? DefaultHourlyRate : decimal.Parse(rateInput);
+
+                    decimal salary = CalculateSalary(hoursWorked, hourlyRate);
+
+                    Console.WriteLine($"{workerName}, your salary for this week is ${salary:0.00}.");
                 }
-                Console.WriteLine(i);
-            }
-            for (int i = 0; i <= 10; i++)
-            {
-                if (i == 5)
+                else
                 {
-                    continue;
+                    Console.WriteLine("Invalid input for hours worked. Please enter a non-negative number.");
+                    i--; // Retry input for the current worker
                 }
-                Console.WriteLine(i);
             }
-            //Module 4
-            Console.WriteLine("Module 5");
-            static void Welcome()
-            {
-                Console.WriteLine("Welcome to Florida");
-                Console.WriteLine("\n Enjoy your Vacation here in Florida!");
-                Console.WriteLine("Thank you");
-            }
-            Welcome();
-            static void WelcomeHome(string name)
-            {
-                Console.WriteLine("Welcome to Florida " + name);
-            }
-            WelcomeHome("Arniel Repuela");
-            static int flight(int year, int age)
-            {
-                int yourAge = year - age;
-                return yourAge;
-            }
-            int myFlight = flight(2023, 0);
-            Console.WriteLine($"Years of my Flight {myFlight}");
         }
+        else
+        {
+            Console.WriteLine("Invalid input for the number of workers. Please enter a positive integer.");
+        }
+
+        Console.WriteLine("\nThank you for using the Construction Salary Calculator!");
+    }
+
+    static decimal CalculateSalary(int hoursWorked, decimal hourlyRate)
+    {
+        decimal salary = hoursWorked * hourlyRate;
+        return salary;
     }
 }
